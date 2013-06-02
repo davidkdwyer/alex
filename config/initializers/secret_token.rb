972058e5696eeffbe7647bb355ca38ca8ce05e58
david dwyer <davidkdwyer@gmail.com>
2013-06-02 09:05:11 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alex::Application.config.secret_token = '8b3fd98d87a803824be19f6eac4e43d9b043384e7656883f0021899ac00ce82a813de4ec5325bfe0d9e60472fb6cd0a6a465194b6b7a354484cb20ade922f2b4'
